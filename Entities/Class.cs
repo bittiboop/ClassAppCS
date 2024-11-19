071daf5893c9c44cb620e84a313cdc0ac6d4c038
bittiboop <sstrelkova15@gmail.com>
2024-11-19 20:10:10 +0200

    added ex2

--- Entities/Class.cs
+++ Entities/Class.cs
@@ -3,6 +3,36 @@ namespace ClassAppCS.Entities
 {
     internal class Class
     {
+        public static bool IsPhibonacci(int n)
+        {
+            if (n < 0) return false;
 
+            return (IsPerfectSquare(5 * n * n + 4) || IsPerfectSquare(5 * n * n - 4));
+        }
+        private static bool IsPerfectSquare(int x)
+        {
+            int sqrt = (int)Math.Sqrt(x);
+            return sqrt * sqrt == x;
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        public static bool IsPhibonacciAndPrime(int n)
+        {
+
+            if (IsPhibonacci(n) && IsPrime(n))
+            {
+                return true;
+            }
+            return false;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace ClassAppCS
     {
         static void Main(string[] args)
         {
-            
+            int num = 5;
+            bool result = Class.IsPhibonacciAndPrime(num);
+            Console.WriteLine($"Number {num} is a Phibonacci number and prime: {result}");
+
+            num = 14;
+            result = Class.IsPhibonacciAndPrime(num);
+            Console.WriteLine($"Number {num} is a Phibonacci number and prime: {result}");
         }
     }
 }
