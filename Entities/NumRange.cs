927bccdf93b71966a11365e1477f29e9000af153
bittiboop <sstrelkova15@gmail.com>
2024-11-19 19:14:21 +0200

    added ex1

--- Entities/NumRange.cs
+++ Entities/NumRange.cs
@@ -4,7 +4,15 @@ namespace ClassAppCS.Entities
 {
     internal class NumRange
     {
-        
+        public static int Product(int startPr, int endPr)
+        {
+            int product = 1;
+            for (int i = startPr; i <= endPr; i++)
+            {
+                product *= i;
+            }
+            return product;
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace ClassAppCS
     {
         static void Main(string[] args)
         {
-            
+            int start = 1;
+            int end = 5;
+            int product = NumRange.Product(start, end);
+            Console.WriteLine($"Product of numbers from {start} to {end} is {product}");
         }
     }
 }
