4c10c4d82da02f8131c0e01a66436a47f7a9289c
bittiboop <sstrelkova15@gmail.com>
2024-11-19 18:09:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassAppCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
