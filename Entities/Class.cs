689b48a75d2b314b236c8e6ceb6b5f90c4e56acc
bittiboop <sstrelkova15@gmail.com>
2024-11-19 20:51:10 +0200

    ended aud work

--- Entities/Class.cs
+++ Entities/Class.cs
@@ -4,56 +4,62 @@ namespace ClassAppCS.Entities
 {
     internal class Class
     {
-        public enum District
+        public enum Position
         {
-            Center,
-            North,
-            South,
-            East,
-            West
+            Guardian,
+            SMM,
+            Manager,
+            Developer,
+            Designer
         }
 
-        public class City
+        public class Worker
         {
             private string name;
-            private string country;
-            private int population;
-            private string phoneCode;
-            private List<District> districts;
+            private string surname;
+            private string birthdate;
+            private string email;
+            private string phone;
+            private string responsibilities;
+            private List<Position> positions;
 
-            public City(string name, string country, int population, string phoneCode, List<District> districts)
+            public Worker(string name, string surname, string birthdate, string email, string phone, string responsibilities, List<Position> positions)
             {
                 this.name = name;
-                this.country = country;
-                this.population = population;
-                this.phoneCode = phoneCode;
-                this.districts = districts ?? new List<District>();
+                this.surname = surname;
+                this.birthdate = birthdate;
+                this.email = email;
+                this.phone = phone;
+                this.responsibilities = responsibilities;
+                this.positions = positions ?? new List<Position>();
             }
-
             public void SetName(string name) => this.name = name;
-            public void SetCountry(string country) => this.country = country;
-            public void SetPopulation(int population) => this.population = population;
-            public void SetPhoneCode(string phoneCode) => this.phoneCode = phoneCode;
-            public void SetDistricts(List<District> districts) => this.districts = districts;
+            public void SetSurname(string surname) => this.surname = surname;
+            public void SetBirthdate(string birthdate) => this.birthdate = birthdate;
+            public void SetEmail(string email) => this.email = email;
+            public void SetPhone(string phone) => this.phone = phone;
+            public void SetResponsibilities(string responsibilities) => this.responsibilities = responsibilities;
+            public void SetPositions(List<Position> positions) => this.positions = positions;
 
-            public string GetName() => name;
-            public string GetCountry() => country;
-            public int GetPopulation() => population;
-            public string GetPhoneCode() => phoneCode;
-            public List<District> GetDistricts() => districts;
 
-            public void DisplayInfo()
-            {
-                Console.WriteLine($"City: {name}");
-                Console.WriteLine($"Country: {country}");
-                Console.WriteLine($"Population: {population}");
-                Console.WriteLine($"PhCode: {phoneCode}");
-                Console.WriteLine("Districts:");
-                foreach (var district in districts)
-                {
-                    Console.WriteLine($"- {district}");
-                }
-            }
+            //    public string GetName() => name;
+            //    public string GetCountry() => country;
+            //    public int GetPopulation() => population;
+            //    public string GetPhoneCode() => phoneCode;
+            //    public List<District> GetDistricts() => districts;
+
+            //    public void DisplayInfo()
+            //    {
+            //        Console.WriteLine($"City: {name}");
+            //        Console.WriteLine($"Country: {country}");
+            //        Console.WriteLine($"Population: {population}");
+            //        Console.WriteLine($"PhCode: {phoneCode}");
+            //        Console.WriteLine("Districts:");
+            //        foreach (var district in districts)
+            //        {
+            //            Console.WriteLine($"- {district}");
+            //        }
+            //    }
         }
     }
 }
